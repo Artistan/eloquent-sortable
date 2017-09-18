5ce811e3a656ce5681ecbe6989a5063631ff086c
Charles Peterson <artistan@gmail.com>
2017-09-18 13:20:14 -0500

    Update DummyWithSoftDeletes.php

--- tests/DummyWithSoftDeletes.php
+++ tests/DummyWithSoftDeletes.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Spatie\EloquentSortable\Test;
+namespace Artistan\EloquentSortable\Test;
 
-use Spatie\EloquentSortable\Sortable;
+use Artistan\EloquentSortable\Sortable;
 use Illuminate\Database\Eloquent\Model;
-use Spatie\EloquentSortable\SortableTrait;
+use Artistan\EloquentSortable\SortableTrait;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DummyWithSoftDeletes extends Model implements Sortable
